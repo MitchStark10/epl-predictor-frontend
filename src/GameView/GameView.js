856c0d87b02d3f90bea4b6fdb00ce03716568851
Mitch Stark <Mitch.Stark.Github@gmail.com>
2019-03-17 10:39:29 -0400

    determine background color based on prediction results - Mitch

--- src/GameView/GameView.js
+++ src/GameView/GameView.js
@@ -37,14 +37,31 @@ class GameView extends Component {
     }
 
     renderGames() {
-        var gameHtmlList = [];
+        let gameHtmlList = [];
         console.log("Enter renderGames");
         for (var i = 0; i < this.state.games.length; i++) {
             let game = this.state.games[i];
-            console.log(JSON.stringify(game));
+
+            let actualGoalDifference = game["HomeTeamScore"] - game["AwayTeamScore"];
+            let predictedGoalDifference = game["PredictedHomeTeamScore"] - game["PredictedAwayTeamScore"];
+
+            console.log(actualGoalDifference +  " : " + predictedGoalDifference);
+
+            let backgroundColor;
+
+            if (actualGoalDifference === predictedGoalDifference) {
+                backgroundColor = "green";
+            } else if ((actualGoalDifference > 0 && predictedGoalDifference > 0) || (actualGoalDifference < 0 && predictedGoalDifference < 0)) {
+                backgroundColor = "lightblue";
+            } else {
+                backgroundColor = "red";
+            }
+
+            let backgroundStyle = {backgroundColor: backgroundColor};
+
             gameHtmlList.push((
-                <div classNam="gameInfo" id={i}>
-                    <p>{game["GameDate"]}: {game["HomeTeam"]} {game["HomeTeamScore"]} ({game["PredictedHomeTeamScore"]}) - {game["AwayTeamScore"]} ({game["PredictedAwayTeamScore"]}) {game["AwayTeam"]}</p>
+                <div className="gameInfo" key={i}>
+                    <p style={backgroundStyle}>{game["GameDate"]}: {game["HomeTeam"]} {game["HomeTeamScore"]} ({game["PredictedHomeTeamScore"]}) - {game["AwayTeamScore"]} ({game["PredictedAwayTeamScore"]}) {game["AwayTeam"]}</p>
                 </div>
             ));
         }
