7ec7349993c7f00338cb6cffed2ced70f5390e94
Mitch Stark <Mitch.Stark.Github@gmail.com>
2019-05-29 22:36:44 -0400

    committing old working version - Mitch

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 const routes = require('./routes');
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "http://localhost:5000");
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
