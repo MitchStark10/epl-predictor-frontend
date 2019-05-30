b215ecd400cfe0de52ca8e90a92b6c33094047a8
Mitch Stark <Mitch.Stark.Github@gmail.com>
2019-05-30 17:49:42 -0400

    prod build - Mitch

--- server/server.js
+++ server/server.js
@@ -2,15 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-
+const path = require("path");
 const routes = require('./routes');
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", process.env.CORS_ACCEPT_HOST);
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(routes);
+
 
 //Retrieve All Games and return as JSON Array
 app.get('/retrieveAllGames', (req, res) => {
@@ -32,5 +25,11 @@ app.get('/retrieveAllGames', (req, res) => {
 app.get('/retrievePredictionSuccessRate', (req, res) => {
     PredictionSuccessRateService.retrievePredictionRates(res);
 })
+
+if (process.env.SCOREMASTER_ENV === "PROD") {
+    app.use("/", express.static(path.join(__dirname, "../build/")));
+}
+
+app.use(routes);
 
 app.listen(process.env.PORT || 8080, () => console.log('EPL Predictor Server app listening on port 8080!'))
