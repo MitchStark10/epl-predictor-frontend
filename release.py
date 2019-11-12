e75e9184fa308f2be2160c22a791fb770dc8cb3f
Mitch Stark <Mitch.Stark.Github@gmail.com>
2019-11-12 16:40:42 -0500

    add in initial release script, still needs to be tested - Mitch

--- release.py
+++ release.py
@@ -1,1 +1,53 @@
 #Release Script to tag and deploy the latest build
+import os
+import json
+
+releaseTypes = ["major", "minor", "patch"]
+
+#Determine what type of release this is
+releaseType = input("Type of release (Major/Minor/Patch):")
+
+if not (releaseType.lower(0 in releaseTypes):
+	raise Exception("Unknown release type: " + releaseType)
+
+#Checkout master
+os.system("git checkout master")
+
+#Merge develop
+os.system("git merge develop")
+
+newPackageVersion = ""
+#Update version number
+with open ("package.json") as file:
+	packageInfo = json.load(file)
+	packageVersionNumbers = packageInfo['version'].split('.')
+
+	if releaseType.lower() == "major":
+		packageVersionNumbers[0] = str(int(packageVersionNumbers[0] + 1))
+	elif releaseType.lower() == "minor":
+		packageVersionNumbers[1] = str(int(packageVersionNumbers[1] + 1))
+	else:
+		packageVersionNumbers[2] = str(int(packageVersionNumbers[2] + 1))
+
+print("Updated package info: " + str(packageInfo))
+
+packageJsonFile = open("package.json", "w")
+packageJsonFile.write(json.dumps(packageInfo, indent=4))
+
+#Commit
+os.system('git commit -m "release version ' + newPackageVersion + '"')
+
+#Tag version number
+os.system('git tag v' + newPackageVersionNumber)
+
+#Push commit
+os.system('git push -u origin master')
+
+#Push tags
+os.system('git push --tags')
+
+#Checkout develop
+os.system('git checkout develop')
+
+#Checkout master
+os.system('git checkout master')
