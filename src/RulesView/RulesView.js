42cb7a514f975e91fd7a00366c3df3b7646419d7
Mitch Stark <mitch.stark@tekton.com>
2020-12-13 18:07:45 -0500

    began working on rules page and eslint

---
+++ src/RulesView/RulesView.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class RulesView extends Component {
+    render() {
+        return (
+            <h1>Rules</h1>
+        );
+    }
+}
+
+export default RulesView;
