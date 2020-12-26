b5b246b16b9ef9cf73c5753de63136f82da7cd66
Mitch Stark <MitchStark10@gmail.com>
2020-12-26 08:59:36 -0600

    update about view

--- src/AboutView/AboutView.js
+++ src/AboutView/AboutView.js
@@ -11,15 +11,11 @@ class AboutView extends Component {
                     <h4>Author: Mitch Stark</h4>
                 </div>
                 <p className="AboutParagraph">
-                    Welcome to ScoreMaster! This site provides a one stop shop for all things EPL.
-                    Whether you are just looking to keep track of your predictions in the league,
-                    looking to read other's thoughts on an upcoming game, or find a spot to host
-                    your blog about your favorite team or league, it can all be done here. We
-                    welcome all lovers of the game, and are excited to have you here!
+                    Welcome to ScoreMaster! This site provides a fun way to compete and interact with other MLS fans around the globe. You will have the opportuntity to prove your knowledge around MLS and have an opportunity to win prizes while doing so. Enjoy!
                 </p>
                 <p className="AboutParagraph">
                     If you have any feature suggestions, any games you'd like to see, or any issues, 
-                    feel free to contact mitchellstark@sbcglobal.net!
+                    feel free to contact MitchStark10@gmail.com!
                 </p>
             </div>
         );
